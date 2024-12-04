27712fe80bb3d4549c43d9797ed350c5ff8c1cb5
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2024-12-04 08:21:32 +0100

    adding delete of user

---
+++ CQRS.APPLICATION/Features/User/Commands/DeleteUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.CORE.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.APPLICATION.Features.User.Commands
+{
+    public record DeleteUserCommand(string appUserId) : IRequest<bool>;
+
+    public class DeleteUserCommandHandler(IUserRepository userRepository) : IRequestHandler<DeleteUserCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellation)
+        {
+            return await userRepository.DeleteUserAsync(request.appUserId);
+        }
+    }
+}
